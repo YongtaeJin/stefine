1666b27665187ef20d1211e3c64405e2fd2bef7e
YongtaeJin <freview@nate.com>
2023-08-10 11:32:40 +0900

    f230810

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="보유주식 수" v-model="stock.m_su1" readonly hide-details /></v-col><v-col cols="4"/>      
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="1주 취득가액 " v-model="stock.m_su3" readonly hide-details /></v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="신청가능 주식수" v-model="stock.m_su2" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" v-model="sval" readonly hide-details /></v-col><v-col cols="4"/> 
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" v-model="stock.m_su4" readonly hide-details /></v-col><v-col cols="4"/> 
       
       <v-col cols="4"/><v-col><v-btn block color="primary" @click="doc('doc1')" :loading="isLoading">자기주식취득에 관한 통지서</v-btn> </v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col><v-btn block color="primary" @click="doc('doc2')" :loading="isLoading">주식양도신청서</v-btn> </v-col><v-col cols="4"/>

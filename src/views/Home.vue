f605a3121599f7e403142b9bc491c101ec433ba2
Yongtae JIN <freview@nate.com>
2026-05-18 11:36:45 +0900

    260518_1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,12 +12,12 @@
 
     <v-row v-else class="text-center">
       
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="성명" v-model="stock.N_NAME" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="주민번호" v-model="stock.I_RESNO" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="보유주식 수" v-model="stock.M_SU1" readonly hide-details /></v-col><v-col cols="4"/>      
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="1주 취득가액 " v-model="stock.M_SU3" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="신청가능 주식수" v-model="stock.M_SU2" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" v-model="!!stock.M_SU4 ? stock.M_SU4 : stock.t_remark" readonly hide-details /></v-col><v-col cols="4"/> 
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="성명" v-model="stock.n_name" readonly hide-details /></v-col><v-col cols="4"/>
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="주민번호" v-model="stock.i_resno" readonly hide-details /></v-col><v-col cols="4"/>
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="보유주식 수" v-model="stock.m_su1" readonly hide-details /></v-col><v-col cols="4"/>      
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="1주 취득가액 " v-model="stock.m_su3" readonly hide-details /></v-col><v-col cols="4"/>
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="신청가능 주식수" v-model="stock.m_su2" readonly hide-details /></v-col><v-col cols="4"/>
+      <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" v-model="!!stock.m_su4 ? stock.m_su4 : stock.t_remark" readonly hide-details /></v-col><v-col cols="4"/> 
       <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc1')" :loading="isLoading">자기주식취득에 관한 통지서</v-btn> </v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc2')" :loading="isLoading">주식양도신청서</v-btn> </v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc4')" :loading="isLoading">위임장</v-btn> </v-col><v-col cols="4"/>      

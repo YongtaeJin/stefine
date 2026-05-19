5d3a326957fbb2f210a194d3ca96981f676fcb83
Yongtae JIN <freview@nate.com>
2026-05-19 17:39:05 +0900

    최대신청가능수

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,13 +11,20 @@
     </v-row>
 
     <v-row v-else class="text-center">
-      
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="성명" v-model="stock.N_NAME" readonly hide-details /></v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="주민번호" v-model="stock.I_RESNO" readonly hide-details /></v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="보유주식 수" v-model="stock.M_SU1" readonly hide-details /></v-col><v-col cols="4"/>      
       <v-col cols="4"/><v-col cols="4"> <v-text-field label="1주 취득가액 " v-model="stock.M_SU3" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="신청가능 주식수" v-model="stock.M_SU2" readonly hide-details /></v-col><v-col cols="4"/>
-      <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" :model-value="stock.M_SU4 || stock.T_REMARK" readonly  hide-details /></v-col><v-col cols="4"/> 
+      <!-- <v-col cols="4"/><v-col cols="4"> <v-text-field label="신청가능 주식수" v-model="stock.M_SU2" readonly hide-details /></v-col><v-col cols="4"/> -->
+      <v-col cols="4"/><v-col cols="4"> 
+        <v-text-field v-if="stock.M_SU1 < MAX_SU" label="신청가능 주식수" v-model="stock.M_SU2" readonly hide-details />
+        <v-text-field v-else label="신청가능 주식수" v-model="MAX_SU" readonly hide-details />
+      </v-col><v-col cols="4"/>
+      <!-- <v-col cols="4"/><v-col cols="4"> <v-text-field label="양도계약 성립주식수" :model-value="stock.M_SU4 > 0 ? stock.M_SU4 : stock.T_REMARK" readonly  hide-details /></v-col><v-col cols="4"/>  -->
+      <v-col cols="4"/><v-col cols="4"> 
+        <v-text-field v-if="stock.M_SU4 > 0" label="양도계약 성립주식수" v-model="stock.T_REMARK" readonly  hide-details />
+        <v-text-field v-else label="양도계약 성립주식수" v-model="stock.T_REMARK" readonly  hide-details />
+      </v-col><v-col cols="4"/> 
       <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc1')" :loading="isLoading">자기주식취득에 관한 통지서</v-btn> </v-col><v-col cols="4"/>
       <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc2')" :loading="isLoading">주식양도신청서</v-btn> </v-col><v-col cols="4"/>
       <!-- <v-col cols="4"/><v-col cols="4"><v-btn block color="primary" @click="doc('doc4')" :loading="isLoading">위임장</v-btn> </v-col><v-col cols="4"/>       -->
@@ -50,9 +57,10 @@ export default {
       valid: true,
       nameChk: false,
       form: {n_name:"", i_resno: ""},
-      stock: {I_RESNO:"", N_NAME:"", A_UN:0, M_SU1:0, M_SU2:0, M_SU3:0, M_SU4:0, A_AMT:0, F_GUN:""},     
+      stock: {I_RESNO:"", N_NAME:"", A_UN:0, M_SU1:0, M_SU2:0, M_SU3:0, M_SU4:0, A_AMT:0, T_REMARK: "", F_GUN:""},     
       // stock: {i_resno:"", n_name:"", a_un:0, m_su1:0, m_su2:0, m_su3:0, m_su4:0, a_amt:0, f_gun:""},     
       sval: "(2023년 8월 7일 이후 확인 가능)",
+      MAX_SU : 13424,
     }
 	},
 	title() {
